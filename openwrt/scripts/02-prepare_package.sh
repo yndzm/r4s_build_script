4e1c000af6af337f5f4ff90725fd41c80fdd1bc8
Masaaki <147675004+yndzm@users.noreply.github.com>
2024-11-20 23:46:34 +0800

    Update 02-prepare_package.sh

--- openwrt/scripts/02-prepare_package.sh
+++ openwrt/scripts/02-prepare_package.sh
@@ -14,6 +14,12 @@ git clone https://$github/sbwml/default-settings package/new/default-settings -b
 # luci-app-filemanager
 git clone https://$github/sbwml/luci-app-filemanager package/new/luci-app-filemanager
 
+# OpenClash
+git clone https://$github/vernesong/OpenClash package/new/luci-app-openclash
+
+# luci-app-adguardhome
+git clone https://$github.com/chenmozhijin/luci-app-adguardhome package/new/luci-app-adguardhome
+
 # luci-app-webdav
 git clone https://$github/sbwml/luci-app-webdav package/new/luci-app-webdav
 
