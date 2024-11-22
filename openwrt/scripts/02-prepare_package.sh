dac2f020af020b901f23b8b385e04718980a27c4
Masaaki <147675004+yndzm@users.noreply.github.com>
2024-11-23 07:20:42 +0800

    Update 02-prepare_package.sh

--- openwrt/scripts/02-prepare_package.sh
+++ openwrt/scripts/02-prepare_package.sh
@@ -18,8 +18,7 @@ git clone https://$github/sbwml/luci-app-filemanager package/new/luci-app-filema
 git clone https://$github/vernesong/OpenClash package/new/luci-app-openclash
 
 # luci-app-adguardhome
-# rm -rf feeds/packages/net/adguardhome
-# git clone https://$github/chenmozhijin/luci-app-adguardhome package/new/luci-app-adguardhome
+git clone https://$github/chenmozhijin/luci-app-adguardhome package/new/luci-app-adguardhome
 
 # luci-app-ddns-go
 git clone https://$github/sirpdboy/luci-app-ddns-go package/new/luci-app-ddns-go
