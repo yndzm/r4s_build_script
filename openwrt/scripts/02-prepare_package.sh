d3a7445a8e4882d8a88dab07791432e35ef035cb
Masaaki <147675004+yndzm@users.noreply.github.com>
2024-11-26 20:35:00 +0800

    Update 02-prepare_package.sh

--- openwrt/scripts/02-prepare_package.sh
+++ openwrt/scripts/02-prepare_package.sh
@@ -23,14 +23,17 @@ git clone https://$github/chenmozhijin/luci-app-adguardhome package/new/luci-app
 # luci-app-ddns-go
 git clone https://$github/sirpdboy/luci-app-ddns-go package/new/luci-app-ddns-go
 
+# luci-app-lucky
+git clone https://$github/sirpdboy/luci-app-lucky package/new/luci-app-ddns-go
+
 # luci-app-webdav
-git clone https://$github/sbwml/luci-app-webdav package/new/luci-app-webdav
+#git clone https://$github/sbwml/luci-app-webdav package/new/luci-app-webdav
 
 # ddns - fix boot
-sed -i '/boot()/,+2d' feeds/packages/net/ddns-scripts/files/etc/init.d/ddns
+#sed -i '/boot()/,+2d' feeds/packages/net/ddns-scripts/files/etc/init.d/ddns
 
 # nlbwmon - disable syslog
-sed -i 's/stderr 1/stderr 0/g' feeds/packages/net/nlbwmon/files/nlbwmon.init
+#sed -i 's/stderr 1/stderr 0/g' feeds/packages/net/nlbwmon/files/nlbwmon.init
 
 # pcre - 8.45
 mkdir -p package/libs/pcre
@@ -59,16 +62,16 @@ curl -s $mirror/openwrt/patch/luci/applications/luci-app-frpc/001-luci-app-frpc-
 curl -s $mirror/openwrt/patch/luci/applications/luci-app-frpc/002-luci-app-frpc-add-enable-flag.patch | patch -p1
 
 # natmap
-pushd feeds/luci
-    curl -s $mirror/openwrt/patch/luci/applications/luci-app-natmap/0001-luci-app-natmap-add-default-STUN-server-lists.patch | patch -p1
-popd
+#pushd feeds/luci
+    #curl -s $mirror/openwrt/patch/luci/applications/luci-app-natmap/0001-luci-app-natmap-add-default-STUN-server-lists.patch | patch -p1
+#popd
 
 # samba4 - bump version
-rm -rf feeds/packages/net/samba4
-git clone https://$github/sbwml/feeds_packages_net_samba4 feeds/packages/net/samba4
+#rm -rf feeds/packages/net/samba4
+#git clone https://$github/sbwml/feeds_packages_net_samba4 feeds/packages/net/samba4
 # liburing - 2.7 (samba-4.21.0)
-rm -rf feeds/packages/libs/liburing
-git clone https://$github/sbwml/feeds_packages_libs_liburing feeds/packages/libs/liburing
+#rm -rf feeds/packages/libs/liburing
+#git clone https://$github/sbwml/feeds_packages_libs_liburing feeds/packages/libs/liburing
 # enable multi-channel
 sed -i '/workgroup/a \\n\t## enable multi-channel' feeds/packages/net/samba4/files/smb.conf.template
 sed -i '/enable multi-channel/a \\tserver multi channel support = yes' feeds/packages/net/samba4/files/smb.conf.template
@@ -86,11 +89,11 @@ sed -i 's/0666/0644/g;s/0777/0755/g' feeds/packages/net/samba4/files/smb.conf.te
 [ "$platform" = "rk3568" ] && sed -i 's#/usr/sbin/smbd -F#/usr/bin/taskset -c 1,0 /usr/sbin/smbd -F#' feeds/packages/net/samba4/files/samba.init
 
 # aria2 & ariaNG
-rm -rf feeds/packages/net/ariang
-rm -rf feeds/luci/applications/luci-app-aria2
-git clone https://$github/sbwml/ariang-nginx package/new/ariang-nginx
-rm -rf feeds/packages/net/aria2
-git clone https://$github/sbwml/feeds_packages_net_aria2 -b 22.03 feeds/packages/net/aria2
+#rm -rf feeds/packages/net/ariang
+#rm -rf feeds/luci/applications/luci-app-aria2
+#git clone https://$github/sbwml/ariang-nginx package/new/ariang-nginx
+#rm -rf feeds/packages/net/aria2
+#git clone https://$github/sbwml/feeds_packages_net_aria2 -b 22.03 feeds/packages/net/aria2
 
 # airconnect
 git clone https://$github/sbwml/luci-app-airconnect package/new/airconnect
@@ -106,18 +109,18 @@ rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
 git clone https://$github/sbwml/openwrt_helloworld package/new/helloworld -b v5
 
 # alist
-rm -rf feeds/packages/net/alist feeds/luci/applications/luci-app-alist
-git clone https://$github/sbwml/openwrt-alist package/new/alist
+#rm -rf feeds/packages/net/alist feeds/luci/applications/luci-app-alist
+#git clone https://$github/sbwml/openwrt-alist package/new/alist
 
 # netdata
-sed -i 's/syslog/none/g' feeds/packages/admin/netdata/files/netdata.conf
+#sed -i 's/syslog/none/g' feeds/packages/admin/netdata/files/netdata.conf
 
 # qBittorrent
-git clone https://$github/sbwml/luci-app-qbittorrent package/new/qbittorrent
+#git clone https://$github/sbwml/luci-app-qbittorrent package/new/qbittorrent
 
 # unblockneteasemusic
-git clone https://$github/UnblockNeteaseMusic/luci-app-unblockneteasemusic package/new/luci-app-unblockneteasemusic
-sed -i 's/解除网易云音乐播放限制/网易云音乐解锁/g' package/new/luci-app-unblockneteasemusic/root/usr/share/luci/menu.d/luci-app-unblockneteasemusic.json
+#git clone https://$github/UnblockNeteaseMusic/luci-app-unblockneteasemusic package/new/luci-app-unblockneteasemusic
+#sed -i 's/解除网易云音乐播放限制/网易云音乐解锁/g' package/new/luci-app-unblockneteasemusic/root/usr/share/luci/menu.d/luci-app-unblockneteasemusic.json
 
 # Theme
 git clone --depth 1 https://$github/sbwml/luci-theme-argon package/new/luci-theme-argon
@@ -132,8 +135,8 @@ git clone https://$github/sbwml/OpenAppFilter --depth=1 package/new/OpenAppFilte
 sed -i "s/D_GNU_SOURCE/D_GNU_SOURCE -funroll-loops/g" feeds/packages/net/iperf3/Makefile
 
 # nlbwmon
-sed -i 's/services/network/g' feeds/luci/applications/luci-app-nlbwmon/root/usr/share/luci/menu.d/luci-app-nlbwmon.json
-sed -i 's/services/network/g' feeds/luci/applications/luci-app-nlbwmon/htdocs/luci-static/resources/view/nlbw/config.js
+#sed -i 's/services/network/g' feeds/luci/applications/luci-app-nlbwmon/root/usr/share/luci/menu.d/luci-app-nlbwmon.json
+#sed -i 's/services/network/g' feeds/luci/applications/luci-app-nlbwmon/htdocs/luci-static/resources/view/nlbw/config.js
 
 # mentohust
 git clone https://github.com/sbwml/luci-app-mentohust package/new/mentohust
@@ -142,26 +145,26 @@ git clone https://github.com/sbwml/luci-app-mentohust package/new/mentohust
 rm -rf feeds/packages/utils/coremark
 git clone https://$github/yndzm/openwrt_pkgs package/new/custom --depth=1
 # coremark - prebuilt with gcc15
-if [ "$platform" = "rk3568" ]; then
-    curl -s $mirror/openwrt/patch/coremark/coremark.aarch64-4-threads > package/new/custom/coremark/src/musl/coremark.aarch64
-elif [ "$platform" = "rk3399" ]; then
-    curl -s $mirror/openwrt/patch/coremark/coremark.aarch64-6-threads > package/new/custom/coremark/src/musl/coremark.aarch64
-elif [ "$platform" = "armv8" ]; then
-    curl -s $mirror/openwrt/patch/coremark/coremark.aarch64-16-threads > package/new/custom/coremark/src/musl/coremark.aarch64
-fi
+#if [ "$platform" = "rk3568" ]; then
+    #curl -s $mirror/openwrt/patch/coremark/coremark.aarch64-4-threads > package/new/custom/coremark/src/musl/coremark.aarch64
+#elif [ "$platform" = "rk3399" ]; then
+    #curl -s $mirror/openwrt/patch/coremark/coremark.aarch64-6-threads > package/new/custom/coremark/src/musl/coremark.aarch64
+#elif [ "$platform" = "armv8" ]; then
+    #curl -s $mirror/openwrt/patch/coremark/coremark.aarch64-16-threads > package/new/custom/coremark/src/musl/coremark.aarch64
+#fi
 
 # luci-compat - fix translation
 sed -i 's/<%:Up%>/<%:Move up%>/g' feeds/luci/modules/luci-compat/luasrc/view/cbi/tblsection.htm
 sed -i 's/<%:Down%>/<%:Move down%>/g' feeds/luci/modules/luci-compat/luasrc/view/cbi/tblsection.htm
 
 # frpc translation
-sed -i 's,发送,Transmission,g' feeds/luci/applications/luci-app-transmission/po/zh_Hans/transmission.po
-sed -i 's,frp 服务器,FRP 服务器,g' feeds/luci/applications/luci-app-frps/po/zh_Hans/frps.po
-sed -i 's,frp 客户端,FRP 客户端,g' feeds/luci/applications/luci-app-frpc/po/zh_Hans/frpc.po
+#sed -i 's,发送,Transmission,g' feeds/luci/applications/luci-app-transmission/po/zh_Hans/transmission.po
+#sed -i 's,frp 服务器,FRP 服务器,g' feeds/luci/applications/luci-app-frps/po/zh_Hans/frps.po
+#sed -i 's,frp 客户端,FRP 客户端,g' feeds/luci/applications/luci-app-frpc/po/zh_Hans/frpc.po
 
 # SQM Translation
-mkdir -p feeds/packages/net/sqm-scripts/patches
-curl -s $mirror/openwrt/patch/sqm/001-help-translation.patch > feeds/packages/net/sqm-scripts/patches/001-help-translation.patch
+#mkdir -p feeds/packages/net/sqm-scripts/patches
+#curl -s $mirror/openwrt/patch/sqm/001-help-translation.patch > feeds/packages/net/sqm-scripts/patches/001-help-translation.patch
 
 # unzip
 rm -rf feeds/packages/utils/unzip
